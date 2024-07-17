dfa13d772462c893cb096842a11231082a2d7b3f
elsa-kim <ksh96611@naver.com>
2024-07-17 17:42:43 +0900

    백준 18258 큐 구현

--- baekjoon/src/구현/큐/큐2_18258.java
+++ baekjoon/src/구현/큐/큐2_18258.java
@@ -1,5 +1,62 @@
 package 구현.큐;
 
-public class 큐2_18258 {
+import java.util.Scanner;
 
+public class 큐2_18258 {
+	public static void main(String[] args) {
+		int fi = -1;
+		int bi = -1;
+		Scanner sc = new Scanner(System.in);
+		int orderCnt;
+		String order;
+		orderCnt = sc.nextInt();
+		int[] que = new int[orderCnt];
+		while(orderCnt>0) {
+			order=sc.next();
+			switch(order) {
+				case "push":
+					int n;
+					n = sc.nextInt();
+					fi++;
+					que[fi] = n;
+					break;
+				case "pop":
+					if(fi==bi) {
+						System.out.println(-1);
+					}else {
+						bi++;
+						System.out.println(que[bi]);
+						que[bi] = 0;
+					}
+					break;
+				case "size":
+					System.out.println(fi-bi);
+					break;
+				case "empty":
+					if(fi==bi) {
+						System.out.println(1);
+					}else {
+						System.out.println(0);
+					}
+					break;
+				case "front":
+					if(fi==bi) {
+						System.out.println(-1);
+					}else {
+						System.out.println(que[bi+1]);
+					}
+					break;
+				case "back":
+					if (fi==bi) {
+						System.out.println(-1);
+					}else {
+						System.out.println(que[fi]);
+					}
+					break;
+					
+			}
+			orderCnt--;
+		}
+		
+	}
 }
