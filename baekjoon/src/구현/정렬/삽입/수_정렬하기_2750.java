e6b670c8a321092c60be1a93fe55c9956719af32
elsa-kim <ksh96611@naver.com>
2024-07-18 17:43:38 +0900

    백준 수 정렬하기 2750 삽입ver 진행중

--- baekjoon/src/구현/정렬/삽입/수_정렬하기_2750.java
+++ baekjoon/src/구현/정렬/삽입/수_정렬하기_2750.java
@@ -1,5 +1,34 @@
 package 구현.정렬.삽입;
 
-public class 수_정렬하기_2750 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class 수_정렬하기_2750 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		
+		for(int i=0;i<size; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for (int i=1; i<size; i++) {
+			int idx = 0;
+			for (int j=0; j<i; j++) {
+				if(arr[i]<arr[j]) {
+					idx = j;
+					break;
+				}
+			}
+			int[] sortingArr = Arrays.copyOfRange(arr, idx,i-1);
+			arr[idx] = arr[i];
+			for(int j=0;j<sortingArr.length;j++) {
+				arr[idx+1] = sortingArr[j];
+			}
+		}
+		for(int i=0; i<size; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 }
