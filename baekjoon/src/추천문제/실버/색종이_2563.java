a332dbb9958895d7ae2d680914499d8a5495599f
elsa-kim <ksh96611@naver.com>
2024-07-25 14:45:40 +0900

    BOJ 색종이 2563

---
+++ baekjoon/src/추천문제/실버/색종이_2563.java
@@ -0,0 +1,35 @@
+package 추천문제.실버;
+
+import java.util.Scanner;
+
+public class 색종이_2563 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cnt = sc.nextInt();
+		int[][] arr = new int[cnt][2];
+		for(int i=0; i<cnt; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+		int sum = cnt*100;
+		for(int i=0; i<cnt; i++) {
+			for(int j=i; j<cnt; j++) {
+				if(arr[i][0]-arr[j][0]>=0 && arr[i][0]-arr[j][0]<10) {
+					if(arr[i][1]-arr[j][1]>=0 && arr[i][1]-arr[j][1]<10) {
+						sum-=((arr[i][1]-arr[j][1])*(10-arr[i][0]+arr[j][0]));
+					}else if(arr[j][1]-arr[i][1]>=0 && arr[j][1]-arr[i][1]<10) {
+						sum-=((arr[j][1]-arr[i][1])*(10-arr[i][0]+arr[j][0]));
+					}
+				}else if(arr[j][0]-arr[i][0]>=0 && arr[j][0]-arr[i][0]<10) {
+					if(arr[i][1]-arr[j][1]>=0 && arr[i][1]-arr[j][1]<10) {
+						sum-=((arr[i][1]-arr[j][1])*(10-arr[j][0]+arr[i][0]));
+					}else if(arr[j][1]-arr[i][1]>=0 && arr[j][1]-arr[i][1]<10) {
+						sum-=((arr[j][1]-arr[i][1])*(10-arr[j][0]+arr[i][0]));
+					}
+				}
+			}
+			
+		}
+		System.out.println(sum);
+	}
+}
