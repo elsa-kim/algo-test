7b2cc1eba39bbd875885781ccc53cc41775112bf
elsa-kim <ksh96611@naver.com>
2024-07-25 09:37:06 +0900

    BOJ 벌집 2292

---
+++ baekjoon/src/추천문제/브론즈/벌집_2292.java
@@ -0,0 +1,20 @@
+package 추천문제.브론즈;
+
+import java.util.Scanner;
+
+public class 벌집_2292 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long target = sc.nextInt();
+		int n=2;
+		if(target==1) {
+			System.out.println(1);
+			return;
+		}else {
+			while(target>(3*(Math.pow(n, 2))-3*n + 1)) {
+				n++;
+			}
+			System.out.println(n);
+		}		
+	}
+}
