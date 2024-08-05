19a7586908acc8d60ff17beadc393d405bd05dcb
elsa-kim <ksh96611@naver.com>
2024-08-05 14:04:42 +0900

    BOJ ATM 11399

---
+++ baekjoon/src/추천문제/실버/ATM_11399.java
@@ -0,0 +1,21 @@
+package 추천문제.실버;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ATM_11399 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cnt = sc.nextInt();
+		int[] arr = new int[cnt];
+		for(int i=0; i<cnt; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		int sum = 0;
+		for(int i=0; i<cnt; i++) {
+			sum+=arr[i]*(cnt-i);
+		}
+		System.out.println(sum);
+	}
+}
