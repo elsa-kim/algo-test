4cd969181b0cc34f2d699e0b5202ff23dcaaa656
elsa-kim <ksh96611@naver.com>
2024-08-06 15:54:09 +0900

    BOJ 개미 10158

--- baekjoon/src/추천문제/실버/개미_10158.java
+++ baekjoon/src/추천문제/실버/개미_10158.java
@@ -8,36 +8,29 @@ import java.io.OutputStreamWriter;
 
 public class 개미_10158 {
 		public static void main(String[] args) throws IOException  {
-			
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 			
 			String[] line1 = br.readLine().split(" ");
-			String[] line2 = br.readLine().split(" ");
 			
 			int w = Integer.parseInt(line1[0]);
 			int h = Integer.parseInt(line1[1]);
-			int p = Integer.parseInt(line2[0]);
-			int q = Integer.parseInt(line2[1]);
+			line1 = br.readLine().split(" ");
+			int p = Integer.parseInt(line1[0]);
+			int q = Integer.parseInt(line1[1]);
 			int t = Integer.parseInt(br.readLine());
 			
-			int realW = t%(2*w);
-			int realH = t%(2*h);
-			if(realW+p>w) {
-				p=2*w-(realW+p);
-			}else {
-				p+=realW;
-			}
+			p+=t%(2*w);
+			q+=t%(2*h);
 			
-			if(realH+q>h) {
-				q=2*h-(realH+q);
-			}else {
-				q+=realH;
+			if(p>w) {
+				p=Math.abs(2*w-p);
 			}
-			
+			if(q>h) {
+				q=Math.abs(2*h-q);
+			}
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			bw.write(p+" "+q);
 			bw.flush();
 			bw.close();
 		}
-
 }
